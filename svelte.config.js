b6edef773e5f5bf454b6fa48d4c314af117cc012
nathan swanepoel <nathan.swanepoel@students.ittralee.ie>
2026-04-15 22:20:20 +0100

    check origin false

--- svelte.config.js
+++ svelte.config.js
@@ -3,13 +3,16 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
+    preprocess: vitePreprocess(),
 
-	kit: {
-		adapter: adapter({
-			out: 'build' // this is where Render expects the server build
-		})
-	}
+    kit: {
+        adapter: adapter({
+            out: 'build'
+        }),
+        csrf: {
+            checkOrigin: false
+        }
+    }
 };
 
 export default config;
